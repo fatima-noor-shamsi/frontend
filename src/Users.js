b34cc0a138c7522c5c0a36c82a0a5b73b4456c14
fatima-noor-shamsi <fatimanoorshamsi987@gmail.com>
2026-03-08 11:56:16 +0500

    frontend is committing

---
+++ src/Users.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users List</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>{user.name} - {user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Users;
